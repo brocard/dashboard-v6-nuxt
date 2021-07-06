8a3325f7af0f6ffff706841aeeb91c7b2dbe0874
enochndika <enochbeloved@gmail.com>
2021-07-06 02:37:11 +0100

    major changes
    - add global title
    - add documentation
    - replace vuex by provide/inject

--- components/dashboard/provider/Overlay.vue
+++ components/dashboard/provider/Overlay.vue
@@ -3,8 +3,8 @@
   <!--  it also allows to close side navigation when you click outside-->
   <div
     :class="
-      open &&
-      'bg-black fixed h-screen left-0 opacity-40 top-0 w-screen z-30 lg:hidden'
+      state.open &&
+      'bg-black fixed h-screen left-0 opacity-40 top-0 w-screen z-30 lg:bg-transparent'
     "
     @click="toggle"
   />
@@ -13,15 +13,6 @@
 <script>
 export default {
   name: 'Overlay',
-  computed: {
-    open() {
-      return this.$store.state.dashboard.open
-    },
-  },
-  methods: {
-    toggle() {
-      this.$store.commit('dashboard/toggle')
-    },
-  },
+  inject: ['state', 'toggle'],
 }
 </script>
